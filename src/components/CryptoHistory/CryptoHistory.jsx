9c5d85192ce3cb7a5879e386c0505d01e7606914
Liza <lizapikovskaya@gmail.com>
2023-05-27 12:51:05 +0300

    tasks-2-3

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,29 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import {formatTime} from 'helpers/functions';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({items}) => {
+  return <BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
+
+    <tbody>
+      {items.map(({ id, price, amount, date }, idx) => {
+        return (
+    <Tr key={id}>
+      <Td>{idx+1}</Td>
+      <Td>{price}</Td>
+      <Td>{amount}</Td>
+      <Td>{formatTime(date)}</Td>
+    </Tr>
+        )
+      })}
+  
+  </tbody>
+</BaseTable>;
 };
